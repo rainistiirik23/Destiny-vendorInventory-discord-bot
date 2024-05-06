d392a253592462bb1c0d08b1034a7fdd16b90275
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-05-06 16:35:57 +0300

    Added interaction that returns all vendors sales

--- index.js
+++ index.js
@@ -46,6 +46,26 @@ client.once(Events.ClientReady, async (readyClient) => {
   /*  setInterval(() => {
   }, 7000); */
 });
-
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+  const { commandName } = interaction;
+  if (commandName === "vendorsales") {
+    const request = await axios.get("https://localhost:8000/api/allVendorSales");
+    const message = [];
+    request.data.currentVendorSales.forEach((vendorSale) => {
+      message.push(vendorSale.itemName);
+      const perksAsJson = JSON.parse(vendorSale.perks);
+      const perkColumnKeys = Object.keys(perksAsJson);
+      for (let i = 0; i < perkColumnKeys.length; i++) {
+        message.push(perkColumnKeys[i]);
+        perksAsJson[perkColumnKeys[i]].forEach((perk) => {
+          message.push(perk.perkName);
+        });
+      }
+    });
+    console.log(request.data.currentVendorSales);
+    interaction.reply("Banshee is selling".concat(...message));
+  }
+});
 // Log in to Discord with your client's token
 client.login(Token);
