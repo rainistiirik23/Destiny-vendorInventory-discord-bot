3bd67d955d47f0b870e32682a1555f61b4538f0f
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-06-09 22:22:53 +0300

    more text format

--- Commands/utility/getCurrentVendorSales.js
+++ Commands/utility/getCurrentVendorSales.js
@@ -4,18 +4,22 @@ async function getAllVendorSales(interaction, commandName) {
     const request = await axios.get("https://localhost:8000/api/currentVendorSales");
     const message = [];
     request.data.currentVendorSales.forEach((vendorSale) => {
-      message.push(vendorSale.itemName);
+      message.push(`\n${vendorSale.itemName}`);
       const perksAsJson = JSON.parse(vendorSale.perks);
       const perkColumnKeys = Object.keys(perksAsJson);
       for (let i = 0; i < perkColumnKeys.length; i++) {
-        message.push(perkColumnKeys[i]);
-        perksAsJson[perkColumnKeys[i]].forEach((perk) => {
-          message.push(perk.perkName);
+        message.push(`\nPerk column ${i + 1}`);
+        perksAsJson[perkColumnKeys[i]].forEach((perk, perkIndex) => {
+          if (perkIndex === 0) {
+            message.push(`\n\t\v${perk.perkName}`);
+            return;
+          }
+          message.push(`  \v${perk.perkName}`);
         });
       }
     });
     console.log(request.data.currentVendorSales);
-    interaction.reply("Banshee is selling".concat(...message));
+    interaction.reply("Banshee is selling ".concat(...message));
   }
 }
 module.exports = getAllVendorSales;
